199cd738cc62296aa0212892854a77c1c3d9f4d0
Nasit Jay <72692535+jayrnasit200@users.noreply.github.com>
2022-06-02 11:30:10 +0530

    1 api  singol

---
+++ android/app/src/main/kotlin/com/example/api_tutoriyal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.api_tutoriyal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Drawer.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:matcher/matcher.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+String? stringResponse;
+Map? mapResponse;
+Map? dataResponse;
+
+class Drawers extends StatefulWidget {
+  @override
+  State<Drawers> createState() => _DrawersState();
+}
+
+class _DrawersState extends State<Drawers> {
+  Future apicall() async {
+    http.Response response;
+    response = await http.get(Uri.parse('https://reqres.in/api/users/2'));
+    if (response.statusCode == 200) {
+      setState(() {
+        // stringResponse = response.body;
+        mapResponse = json.decode(response.body);
+        dataResponse = mapResponse!['data'];
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    apicall();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Drawer(
+        child: ListView(children: [
+          UserAccountsDrawerHeader(
+            currentAccountPicture:
+                Image.network(dataResponse!['avatar'].toString()),
+            accountName: Text(dataResponse!['first_name'].toString() +
+                ' ' +
+                dataResponse!['last_name'].toString()),
+            accountEmail: Text(dataResponse!['email'].toString()),
+          ),
+          ListTile(title: Text('data is lodding')),
+        ]),
+      ),
+    );
+  }
+}
